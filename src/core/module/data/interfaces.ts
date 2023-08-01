6e54e4e9cd96226fa6ae63d3e84d579681458588
YettiKetchup <yetti.ph@gmail.com>
2023-08-01 13:19:50 +0300

    - Split IModule to small reusable interfaces.

---
+++ src/core/module/data/interfaces.ts
@@ -0,0 +1,11 @@
+export interface IInitable {
+  init(): void;
+}
+
+export interface IUpdatable {
+  update(dt: number): void;
+}
+
+export interface IDestroyable {
+  destroy(): void;
+}
--- src/core/module/index.ts
+++ src/core/module/index.ts
@@ -1,1 +1,2 @@
+export * from './data/interfaces';
 export { IModule } from './module';
