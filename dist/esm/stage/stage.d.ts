467d6cb5f808be4be74cbf8d01b830d8a59bb89b
YettiKetchup <yetti.ph@gmail.com>
2023-08-01 16:23:25 +0300

    - Version 1.0.4.

--- dist/esm/stage/stage.d.ts
+++ dist/esm/stage/stage.d.ts
@@ -4,5 +4,14 @@ export declare abstract class Stage implements IStage {
     init(): void;
     update(dt: number): void;
     destroy(): void;
+    /**
+     * Use to preload assets or data.
+     *
+     * @example
+     * public async preload(): Promise<void> {
+     *  const data = await fetch(api);
+     *  // ...do something.
+     * }
+     */
     abstract preload(): Promise<void>;
 }
