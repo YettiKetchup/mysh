f96ca60da45ae25748ef4e13bae243f5da775a3e
YettiKetchup <yetti.ph@gmail.com>
2023-08-06 14:26:10 +0300

    - Version 1.1.3.

--- dist/cjs/core/collections/combined-entities.collection.d.ts
+++ dist/cjs/core/collections/combined-entities.collection.d.ts
@@ -1,7 +1,13 @@
+import { IComponentFilter } from '../component';
 import { Entity } from '../entity';
 import { EntitiesCollection } from './entities.collection';
+import { SystemEntitiesCollection } from './system-entities.collection';
 export declare class CombinedEntitiesCollection extends EntitiesCollection {
     private _colelctions;
-    get entities(): Entity[];
     constructor(_colelctions: EntitiesCollection[]);
+    add(...entities: Entity[]): void;
+    destroy(entity: Entity): void;
+    get(filter: IComponentFilter): SystemEntitiesCollection<Entity>;
+    private getCombinedEntities;
+    private combineEntities;
 }
--- dist/esm/core/collections/combined-entities.collection.js
+++ dist/esm/core/collections/combined-entities.collection.js
@@ -1,14 +1,37 @@
 import { EntitiesCollection } from './entities.collection';
 export class CombinedEntitiesCollection extends EntitiesCollection {
-    get entities() {
-        const groupedEntities = this._colelctions.map((collection) => collection.entities);
-        const empty = [];
-        const entities = empty.concat(...groupedEntities);
-        return [...this._entities, ...entities];
-    }
     constructor(_colelctions) {
         super();
         this._colelctions = _colelctions;
+    }
+    add(...entities) {
+        this._entities = this.getCombinedEntities();
+        super.add(...entities);
+    }
+    destroy(entity) {
+        const collection = this._colelctions.find((collection) => collection.entities.find((e) => e.id === entity.id));
+        if (collection) {
+            collection.destroy(entity);
+            this._entities = this._entities.filter((e) => e.id !== entity.id);
+            this._entities = this.getCombinedEntities();
+        }
+        else {
+            super.destroy(entity);
+            this._entities = this.getCombinedEntities();
+        }
+    }
+    get(filter) {
+        this._entities = this.getCombinedEntities();
+        return super.get(filter);
+    }
+    getCombinedEntities() {
+        const unique = new Set([...this.entities, ...this.combineEntities()]);
+        return [...unique];
+    }
+    combineEntities() {
+        const groupedEntities = this._colelctions.map((collection) => collection.entities);
+        const empty = [];
+        return empty.concat(...groupedEntities);
     }
 }
 //# sourceMappingURL=combined-entities.collection.js.map
